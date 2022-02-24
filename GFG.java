6f81f551ea06be9e528ae36d2451dfac0514f0ad
Mayank Madhav <mayank.madhav@hotmail.com>
2022-02-25 01:10:25 +0530

    Update GFG.java

--- GFG.java
+++ GFG.java
@@ -1,18 +1,19 @@
-// { Driver Code Starts
-//Initial Template for Java
+package algos.dp.booleanParenthesization;
 
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
 
-class GFG{
-    public static void main(String args[])throws IOException
-    {
+public class GFG {
+    public static void main(String[] args) throws IOException {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(in.readLine());
-        while(t-- > 0){
+        while (t-- > 0) {
             int N = Integer.parseInt(in.readLine());
             String S = in.readLine();
-            
+
             Solution ob = new Solution();
             System.out.println(ob.countWays(N, S));
         }
@@ -22,94 +23,74 @@ class GFG{
 
 //User function Template for Java
 
-class Solution{
-    static int count=0;
-    static int countWays(int N, String S){
-        if(N==0)
+class Solution {
+    static Map<String, Integer> mp;
+
+    static int countWays(int N, String S) {
+        if (N == 0)
             return 0;
-        if(N==1){
-            if("T".equals(S))
+        if (N == 1) {
+            if ("T".equals(S))
                 return 1;
             else
                 return 0;
         }
-        if(N%2==0)
+        if (N % 2 == 0)
             return 0;
         char[] ch = S.toCharArray();
-        if(N==3){
-            boolean op1=(ch[0]=='T'?true:false);
-            boolean op2=(ch[2]=='T'?true:false);
-            if(evaluate(op1,op2,ch[1]))
-                return 1;
-            else
-                return 0;
-        }
-        
-        solve(ch,0,N-1,true);
-        return count;
+        mp = new HashMap<>();
+        return solve(ch, 0, N - 1, true);
+
     }
-    
-    static boolean solve(char[] arr,int beg, int end, boolean required){
-        boolean result=false;
-        if(end==beg)
-            return arr[beg]=='T'?required:!required;
-        
-        for(int i=beg+1;i<end;i=i+2){
-            boolean lt = solve(arr,beg,i-1,true);
-            boolean rt = solve(arr,i+1,end,true);
-            boolean lf = solve(arr,beg,i-1,false);
-            boolean rf = solve(arr,i+1,end,false);
-            switch(arr[i]){
+
+    static int solve(char[] arr, int beg, int end, boolean requiredTrue) {
+
+        if (end == beg)
+            return arr[beg] == 'T' ? (requiredTrue ? 1 : 0) : (!requiredTrue ? 1 : 0);
+
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(beg);
+        sb.append(" ");
+        sb.append(end);
+        sb.append(" ");
+        sb.append(requiredTrue);
+        String temp = sb.toString();
+
+        if (mp.containsKey(temp)) {
+            return mp.get(temp);
+        }
+        int result = 0;
+
+        for (int k = beg + 1; k < end; k = k + 2) {
+            char operator = arr[k];
+            int lt = solve(arr, beg, k - 1, true);
+            int lf = solve(arr, beg, k - 1, false);
+            int rt = solve(arr, k + 1, end, true);
+            int rf = solve(arr, k + 1, end, false);
+            switch (operator) {
                 case '&':
-                    if(required){
-                        result = lt & rt;
-                        if(result)
-                            count++;
-                    }else{
-                        result = (lt & rf)|(lf & rt)|(lf & rf);
-                        if(result)
-                            count++;
-                    }
+                    if (requiredTrue)
+                        result = result + (lt * rt);
+                    else
+                        result = result + (lt * rf) + (lf * rt) + (lf * rf);
                     break;
                 case '|':
-                    if(required){
-                        result = lt | rt;
-                        if(result)
-                            count++;
-                    }else{
-                        result = lf & rf;
-                        if(result)
-                            count++;
-                    }
+                    if (requiredTrue)
+                        result = result + (lt * rf) + (lf * rt) + (lt * rt);
+                    else
+                        result = result + (lf * rf);
                     break;
                 case '^':
-                    if(required){
-                        result = (lt & rf)|(lf & rt);
-                        if(result)
-                            count++;
-                    }else{
-                        result = (lt & rt)|(lf & rf);
-                        if(result)
-                            count++;
-                    }
+                    if (requiredTrue)
+                        result = result + (lt * rf) + (lf * rt);
+                    else
+                        result = result + (lt * rt) + (lf * rf);
                     break;
             }
         }
-        return result;
-    }
-    static boolean evaluate(boolean op1,boolean op2, char operand){
-        boolean result = false;
-        switch(operand){
-                case '|':
-                    result=op1|op2;
-                    break;
-                case '&':
-                    result = op1&op2;
-                    break;
-                case '^':
-                    result = op1^op2;
-                    break;
-            }
-        return result;    
+
+        mp.put(temp, result % 1003);
+        return mp.get(temp);
     }
 }
